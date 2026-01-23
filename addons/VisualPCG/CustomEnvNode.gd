ccaade609dcb5bbcc6be0135663a946ae6b83fe3
mukarramarif <mukarramarif03@gmail.com>
2026-01-23 17:18:09 -0500

    progress setting up UI

---
+++ addons/VisualPCG/CustomEnvNode.gd
@@ -0,0 +1,72 @@
+@tool
+extends Control
+
+var graph_edit: GraphEdit
+var add_node_menu: PopupMenu
+var toolbar: HBoxContainer
+var mouse_pos_for_new_node: Vector2
+
+func _ready() -> void:
+	setup_ui()
+	setup_graph_edit()
+func setup_ui():
+	var vbox = VBoxContainer.new()
+	vbox.anchor_right = 1.0
+	vbox.anchor_bottom = 1.0
+	add_child(vbox)
+	
+	toolbar = HBoxContainer.new()
+	toolbar.custom_minimum_size.y = 40
+	vbox.add_child(toolbar)
+	
+	graph_edit = GraphEdit.new()
+	graph_edit.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	graph_edit.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	vbox.add_child(graph_edit)
+func setup_graph_edit():
+	graph_edit.right_disconnects = true
+	graph_edit.show_zoom_label = true
+	graph_edit.minimap_enabled = true
+	graph_edit.minimap_opacity = 0.5
+	graph_edit.snap_distance = 20
+	
+	graph_edit.connection_request.connect(_on_connection_request)
+	graph_edit.disconnection_request.connect(_on_disconnection_request)
+	graph_edit.delete_nodes_request.connect(_on_delete_nodes_request)
+	graph_edit.popup_request.connect(_on_popup_request)
+	graph_edit.gui_input.connect(_on_graph_gui_input)
+	
+# Signal handlers
+func _on_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
+	# Validate connection types here if needed
+	var from_node_obj = graph_edit.get_node(NodePath(from_node))
+	var to_node_obj = graph_edit.get_node(NodePath(to_node))
+	
+	# Simple type checking (you can expand this)
+	graph_edit.connect_node(from_node, from_port, to_node, to_port)
+	_on_execute_graph()
+
+func _on_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
+	graph_edit.disconnect_node(from_node, from_port, to_node, to_port)
+	_on_execute_graph()
+
+func _on_delete_nodes_request(nodes: Array):
+	for node_name in nodes:
+		var node = graph_edit.get_node(NodePath(node_name))
+		if node:
+			graph_edit.remove_child(node)
+			node.queue_free()
+
+func _on_popup_request(position: Vector2):
+	mouse_pos_for_new_node = (position + graph_edit.scroll_offset) / graph_edit.zoom
+	add_node_menu.position = get_viewport().get_mouse_position()
+	add_node_menu.popup()
+
+func _on_graph_gui_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			mouse_pos_for_new_node = (event.position + graph_edit.scroll_offset) / graph_edit.zoom
+			add_node_menu.position = get_viewport().get_mouse_position()
+			add_node_menu.popup()
+func _on_execute_graph():
+	print("Executing PCG Graph...")
--- addons/VisualPCG/plugin_entry.gd
+++ addons/VisualPCG/plugin_entry.gd
@@ -1,12 +1,19 @@
 @tool
 extends EditorPlugin
 
-
+var pcg_editor_panel: Control
+var pcg_dock: Control
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
+	var pcg_editor_scene = preload("res://addons/VisualPCG/PcgEditScene.tscn")
+	pcg_editor_panel = pcg_editor_scene.instantiate()
+	add_control_to_bottom_panel(pcg_editor_panel, "PCG Visual")
+	
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
+	if pcg_editor_panel:
+		remove_control_from_bottom_panel(pcg_editor_panel)
+		pcg_editor_panel.queue_free()
 	pass
